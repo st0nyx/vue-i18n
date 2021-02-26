5fd204377e8d2d95e7403fff292b4f3f3c3d855b
Bruno Steinmann <bruno.steinmann@gmx.com>
2021-02-26 14:33:21 +0100

    style number formatting

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -17,18 +17,34 @@
 
     <p>{{ $n(100, "currency") }}</p>
 
-
     <p>{{ product.name }}: {{ $n(product.price, "currency") }}</p>
     <p>{{ product.stock }} left in stock</p>
 
+    <div>
+      <i18n-n :value="4567.45" format="currency">
+        <template v-slot:currency="slotProps">
+          <span style="color: red; font-size: 25px; font-weight: bold">{{
+            slotProps.currency
+          }}</span>
+        </template>
+        <template v-slot:integer="slotProps">
+          <span style="color:red; font-size:25px">{{ slotProps.integer }}</span>
+        </template>
+        <template v-slot:decimal="slotProps">
+          <span style="color:blue">{{ slotProps.decimal }}</span>
+        </template>
+        <template v-slot:fraction="slotProps">
+          <span style="color:purple">{{ slotProps.fraction }}</span>
+        </template>
+        <template v-slot:group="slotProps">
+          <span style="color:red; font-size:25px">{{ slotProps.group }}</span>
+        </template>
+      </i18n-n>
+    </div>
 
     <button @click="setLocale('en-GB')" class="button">UK</button>
     <button @click="setLocale('en-US')" class="button">USA</button>
     <button @click="setLocale('es-ES')" class="button">German</button>
-
-
-
-
   </div>
 </template>
 <script>
@@ -55,8 +71,7 @@ export default {
   data() {
     return {
       name: "Stonyx"
-
-    }
+    };
   },
   methods: {
     setLocale(locale) {
@@ -69,6 +84,5 @@ export default {
       return products[this.$i18n.locale];
     }
   }
-
 };
 </script>
