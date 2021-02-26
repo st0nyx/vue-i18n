a643e73950db042d44eaddabe8210acd2017e2de
Bruno Steinmann <bruno.steinmann@gmx.com>
2021-02-26 10:47:13 +0100

    added more lang and make persistent with localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloI18n />
   </div>
 </template>
+<script>
+import HelloI18n from "./components/HelloI18n.vue";
+export default {
+  name: "app",
+  components: {
+    HelloI18n
+  },
+  created() {
+    const locale = localStorage.getItem("locale");
+    if (locale) {
+      this.$i18n.locale = locale;
+    }
+  }
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
-#nav {
-  padding: 30px;
+.button {
+  border: 1px solid black;
+  padding: 8px;
+  margin: 2px;
 }
-
-#nav a {
-  font-weight: bold;
+.button:hover {
   color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,17 +1,20 @@
 <template>
-  <p>{{ $t("hello") }}</p>
+  <div>
+    <p>{{ $t("hello") }}</p>
+    <p>{{ $t("goodbye") }}</p>
+    <button @click="setLocale('en')" class="button">English</button>
+    <button @click="setLocale('es')" class="button">Spanish</button>
+    <button @click="setLocale('de')" class="button">German</button>
+  </div>
 </template>
-
 <script>
 export default {
-  name: "HelloI18n"
+  name: "HelloI18n",
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale;
+      localStorage.setItem("locale", locale);
+    }
+  }
 };
 </script>
-
-<i18n>
-{
-  "en": {
-    "hello": "Hello i18n in SFC!"
-  }
-}
-</i18n>
