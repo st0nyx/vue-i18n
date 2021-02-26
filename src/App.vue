6170dc74acba5179da8477a22d719921412c3eb6
Bruno Steinmann <bruno.steinmann@gmx.com>
2021-02-26 11:00:24 +0100

    getting Locale from the browser

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,8 @@ export default {
     const locale = localStorage.getItem("locale");
     if (locale) {
       this.$i18n.locale = locale;
+    } else if (navigator.language) {
+      this.$i18n.locale = navigator.language.substring(0, 2)
     }
   }
 };
